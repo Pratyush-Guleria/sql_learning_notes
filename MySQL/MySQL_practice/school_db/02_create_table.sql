18620421e01839f7f3f09a42db162ccd967490fe
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-07-14 23:53:46 +0530

    Created Table for school_db

--- MySQL/MySQL_practice/school_db/02_create_table.sql
+++ MySQL/MySQL_practice/school_db/02_create_table.sql
@@ -1,1 +1,19 @@
-02_create_table.sql
+-- 2. Creating table 'student_info' and adding columns like student_id, name, age, marks, department 
+
+-- Using database 'school_db'
+USE school_db;
+
+CREATE TABLE student_info(
+
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    name VARCHAR(100) NOT NULL,
+
+    age INT,
+
+    marks FLOAT,
+
+    department VARCHAR(50) DEFAULT 'General',
+
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);

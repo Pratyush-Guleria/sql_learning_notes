9a4ca0109a86c3f0d62cf0356654c92d2c0a8c67
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-07-14 13:42:22 +0530

    Add SchoolDB database and student_info table setup

---
+++ MySQL/MySQL_practice/01_school_db.sql
@@ -0,0 +1,29 @@
+-- 1. Creating a Data Base 'schooldb'--
+---CREATE DATABASE schooldb;---
+
+-- Using Data Base 
+-- USE schooldb;--
+SHOW TABLES;
+-- 2. Creating Table 'students_info' : student_id, name, age, marks, department with proper columns and constraints --
+CREATE TABLE students_info(
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    marks FLOAT,
+    department VARCHAR(50) DEFAULT 'General',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- 3. Inserting Data of 5 student --
+INSERT INTO students_info(name, age, marks, department)
+VALUES
+        ('Pratyush', 17, 68, 'CSE'),
+        ('Anirudh', 13),
+        ('ChatGPT', 20, 100, 'Civil'),
+        ('Claude', 20, 100, 'CSE'),
+        ('Google Gemini', 10, 99.9);
+
+
+-- 4. Checking Tabel Structure --
+DESC students_info; 

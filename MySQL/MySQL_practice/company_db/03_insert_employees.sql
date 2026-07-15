a1f0ac17eda09060772d18c8f39d5cd5a5edb167
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-07-15 13:57:45 +0530

    Inserted data into table

---
+++ MySQL/MySQL_practice/company_db/03_insert_employees.sql
@@ -0,0 +1,19 @@
+-- Using database 'company_db'
+USE company_db;
+
+-- 3. Insert Data
+
+INSERT INTO employe_info(name, department, salary, experience)
+VALUES
+
+('Google Gemini', 'ML', 10000, 7),
+
+('ChatGPT', 'LLM', 10000, 6.5),
+
+('Claude', 'Senior MLOps Engineer', 90000, 15),
+
+('Pratyush', 'Junior ML Engineer', 5000, 2),
+
+('Raj', 'HR', 1000, 1),
+
+('Amit', 'Sales', 1000, 2);

fe9f9786948f8ad2f49fdde45c200aa4ad81bd62
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-07-16 17:14:21 +0530

    Inserted Data in Tables

---
+++ MySQL/MySQL_practice/academic_db/03_insert_data.sql
@@ -0,0 +1,16 @@
+-- Using database 'academic_db'
+USE academic_db; 
+
+-- Students data
+INSERT INTO students VALUES
+(1, 'Pratyush', 1),
+(2, 'Rahul', 2),
+(3, 'Amit', 1),
+(4, 'Sneha', 3),
+(5, 'Vikram', NULL);
+
+-- Departments data
+INSERT INTO departments VALUES
+(1, 'CSE'),
+(2, 'ECE'),
+(4, 'IT');
